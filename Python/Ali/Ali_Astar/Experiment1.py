50bb0cb3c94a2bab4a14a30f3fe4927642699300
Ali Reza Farid Amin <ali.r.farid@gmail.com>
2018-02-04 14:43:31 +0100

    update four

--- Python/Ali/Ali_Astar/Experiment1.py
+++ Python/Ali/Ali_Astar/Experiment1.py
@@ -10,17 +10,18 @@ import agents
 
 # variable paramter: number of agents
 # fixed paramters: grid_size, number of steps, density vision, and density coefficent
+#  
 
 mean_utility =[]
-agent_numbers = [2,10,100,1000]
-number_of_steps = 1000
+agent_numbers = [1,5,50,200]
+number_of_steps = 500
 
 for n in  agent_numbers:
 
-	my_model = model.Model(n, 50,50)
+	my_model = model.Model(n,50,50,distance_coefficent= 0.0 ,density_coefficent=0,density_vision=0)
 	for t in range(number_of_steps):
 		my_model.step()
-		#print(t)
+		print(n,'agents','step',t)
 
 
 	agent_df = my_model.datacollector.get_agent_vars_dataframe()
@@ -29,10 +30,14 @@ for n in  agent_numbers:
 	all_utility = []
 	for agent in my_model.schedule.agents:
 			if agent.numOfPOIVisits != 0:
-				all_utility.append((agent.utility /agent.numOfPOIVisits) * 100)
-				
+				all_utility.append((agent.utility /agent.numOfPOIVisits))	
 	mean_utility.append(np.mean(all_utility))
 
-
 plt.plot(agent_numbers,mean_utility)
+plt.scatter(agent_numbers,mean_utility)
+plt.xlabel('Number of Agents')
+plt.ylabel('Average Utility')
+plt.title('The effect of number of agents on average utility')
+plt.savefig("Experiment1-DifferentNumOfAgents")
+plt.grid(True)
 plt.show()
--- Python/Ali/Ali_Astar/Experiment2.py
+++ Python/Ali/Ali_Astar/Experiment2.py
@@ -12,16 +12,15 @@ import agents
 # fixed paramters:  number of agents, grid_size, number of steps, density vision, and density coefficent
 
 mean_utility =[]
-agent_numbers = 400
-number_of_steps = 400
+agent_numbers = 200
+number_of_steps = 600
 
-vision_range = [2,4]
+vision_range = [2,4,5]
 for n in  vision_range:
-	print(n)
-	my_model = model.Model(agent_numbers, 50,50,0,2,n)
+	my_model = model.Model(agent_numbers, 50,50,distance_coefficent= 0 ,density_coefficent=5,density_vision= n)
 	for t in range(number_of_steps):
 		my_model.step()
-		print(t)
+		print(n,t)
 	all_numOfPOIVisits =[]
 	all_utility = []
 	for agent in my_model.schedule.agents:
@@ -32,4 +31,11 @@ for n in  vision_range:
 
 
 plt.plot(vision_range,mean_utility)
+plt.scatter(vision_range,mean_utility)
+plt.xlabel('Vision Range')
+plt.ylabel('Average Utility')
+plt.title('The effect of vision on average utility')
+plt.savefig("Experiment2-DifferentVisions-d1-n200")
+plt.grid(True)
+
 plt.show()
